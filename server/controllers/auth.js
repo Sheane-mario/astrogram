732c5f390e93161995ed250f73326e9945fa0260
sheanemario <sheanemario7@gmail.com>
2024-09-01 19:24:04 +0530

    added mongoose user model and implemented backend authenitication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,2 +1,60 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+/* REGISTER */
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            followers,
+            following,
+            location,
+            occupation
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            picturePath,
+            followers,
+            following,
+            location,
+            occupation,
+        });
+
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({msg: 'User does not exist!'});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({msg: 'Invalid credentials!'});
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ user, token });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,48 @@
 import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            min: 3,
+            max: 30,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 3,
+            max: 30,
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 100,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+            max: 1024,
+        },
+        picturePath: {
+            type: String,
+            default: '',
+        },
+        followers: {
+            type: Array,
+            default: [],
+        },
+        following: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        occupation: String,
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+export default User;
